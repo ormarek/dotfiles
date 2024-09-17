1ecfa11f64b6b2df5e55f5c8b27ae816c6772be3
ormarek <16357457+ormarek@users.noreply.github.com>
2024-09-18 01:14:47 +0200

    adjust nvim configuration

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "javascript",
+        "typescript",
+        "css",
+        "gitignore",
+        "graphql",
+        "http",
+        "json",
+        "scss",
+        "sql",
+        "vim",
+        "lua",
+        "python",
+        "rust",
+        "yaml",
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+    },
+  },
+}
