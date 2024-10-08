e80f30f0be6bddfd67da40ad46838149fb0543f9
ormarek <16357457+ormarek@users.noreply.github.com>
2024-10-08 16:30:25 +0200

    cleanup nvim config and adjust for updated plugins

--- .config/nvim/lua/plugins/_extras.lua
+++ .config/nvim/lua/plugins/_extras.lua
@@ -4,6 +4,7 @@ return {
 
   -- editor
   { import = "lazyvim.plugins.extras.editor.telescope" },
+  { import = "lazyvim.plugins.extras.editor.dial" },
 
   -- formatting
   { import = "lazyvim.plugins.extras.formatting.prettier" },
@@ -12,20 +13,23 @@ return {
   { import = "lazyvim.plugins.extras.linting.eslint" },
 
   -- languages
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.lang.elixir" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  -- { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  -- { import = "lazyvim.plugins.extras.lang.elixir" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.terraform" },
-  { import = "lazyvim.plugins.extras.lang.toml" },
 
   -- testing & debugging
   -- { import = "lazyvim.plugins.extras.test.core" },
 
   -- utilities
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+  { import = "lazyvim.plugins.extras.util.dot" },
 }
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,8 +1,44 @@
+-- Define multiple headers
+local headers = {
+  {
+    " ░▒▓██████▓▒░  ░▒▓███████▓▒░  ░▒▓██████████████▓▒░   ░▒▓██████▓▒░  ░▒▓███████▓▒░  ░▒▓████████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░",
+    "░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░",
+    "░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░",
+    "░▒▓█▓▒░░▒▓█▓▒░ ░▒▓███████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓████████▓▒░ ░▒▓███████▓▒░  ░▒▓██████▓▒░   ░▒▓███████▓▒░ ",
+    "░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░",
+    "░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░",
+    " ░▒▓██████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓████████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░",
+  },
+  -- Add more headers here...
+}
+
+-- Function to get a random header
+local function get_random_header()
+  math.randomseed(os.time()) -- Seed the random number generator
+  return headers[math.random(#headers)]
+end
+
 return {
+  -- Dashboard configuration
+  {
+    "nvimdev/dashboard-nvim",
+    opts = function(_, opts)
+      local logo = get_random_header()
+      logo = vim.list_extend(
+        { "", "", "", "", "", "", "", "" }, -- 8 empty lines
+        vim.list_extend(
+          logo,
+          { "", "" } -- 2 empty lines
+        )
+      )
+      opts.config.header = logo
+      return opts
+    end,
+  },
+  -- Colorscheme
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    lazy = false,
     priority = 1000,
     config = function()
       require("catppuccin").setup({
@@ -27,277 +63,7 @@ return {
           types = {},
         },
       })
-      vim.cmd.colorscheme("catppuccin")
+      -- vim.cmd.colorscheme("catppuccin")
     end,
   },
-  {
-    "nvimdev/dashboard-nvim",
-    lazy = false,
-    opts = function()
-      local logo = [[
-
- ░▒▓██████▓▒░  ░▒▓███████▓▒░  ░▒▓██████████████▓▒░   ░▒▓██████▓▒░  ░▒▓███████▓▒░  ░▒▓████████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░ ░▒▓███████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓████████▓▒░ ░▒▓███████▓▒░  ░▒▓██████▓▒░   ░▒▓███████▓▒░  
-░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░ 
- ░▒▓██████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓████████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ 
-                                                                                                                
-                                                                                                                
-    ]]
-
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-
-      local opts = {
-        theme = "doom",
-        hide = {
-          -- this is taken care of by lualine
-          -- enabling this messes up the actual laststatus setting after loading a file
-          statusline = false,
-        },
-        config = {
-          header = vim.split(logo, "\n"),
-          -- stylua: ignore
-          center = {
-            { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
-            { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-            { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
-            { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
-            { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
-            { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-            { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
-            { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
-            { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
-          },
-          footer = function()
-            local stats = require("lazy").stats()
-            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-          end,
-        },
-      }
-
-      for _, button in ipairs(opts.config.center) do
-        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-        button.key_format = "  %s"
-      end
-
-      -- open dashboard after closing lazy
-      if vim.o.filetype == "lazy" then
-        vim.api.nvim_create_autocmd("WinClosed", {
-          pattern = tostring(vim.api.nvim_get_current_win()),
-          once = true,
-          callback = function()
-            vim.schedule(function()
-              vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
-            end)
-          end,
-        })
-      end
-
-      return opts
-    end,
-  },
-  -- messages, cmdline and the popupmenu
-  {
-    "folke/noice.nvim",
-    opts = function(_, opts)
-      table.insert(opts.routes, {
-        filter = {
-          event = "notify",
-          find = "No information available",
-        },
-        opts = { skip = true },
-      })
-      local focused = true
-      vim.api.nvim_create_autocmd("FocusGained", {
-        callback = function()
-          focused = true
-        end,
-      })
-      vim.api.nvim_create_autocmd("FocusLost", {
-        callback = function()
-          focused = false
-        end,
-      })
-      table.insert(opts.routes, 1, {
-        filter = {
-          cond = function()
-            return not focused
-          end,
-        },
-        view = "notify_send",
-        opts = { stop = false },
-      })
-
-      opts.commands = {
-        all = {
-          -- options for the message history that you get with `:Noice`
-          view = "split",
-          opts = { enter = true, format = "details" },
-          filter = {},
-        },
-      }
-
-      opts.presets.lsp_doc_border = true
-    end,
-  },
-
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 5000,
-      background_colour = "#000000",
-      render = "wrapped-compact",
-    },
-  },
-
-  -- buffer line
-  -- {
-  --   "akinsho/bufferline.nvim",
-  --   event = "VeryLazy",
-  --   keys = {
-  --     { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-  --     { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-  --   },
-  --   opts = {
-  --     options = {
-  --       mode = "tabs",
-  --       show_buffer_close_icons = false,
-  --       show_close_icon = false,
-  --     },
-  --   },
-  -- },
-
-  -- filename
-  -- {
-  --   "b0o/incline.nvim",
-  --   dependencies = {},
-  --   event = "BufReadPre",
-  --   priority = 1200,
-  --   config = function()
-  --     local helpers = require("incline.helpers")
-  --     require("incline").setup({
-  --       window = {
-  --         padding = 0,
-  --         margin = { horizontal = 0 },
-  --       },
-  --       render = function(props)
-  --         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-  --         local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
-  --         local modified = vim.bo[props.buf].modified
-  --         local buffer = {
-  --           ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-  --           " ",
-  --           { filename, gui = modified and "bold,italic" or "bold" },
-  --           " ",
-  --           guibg = "#363944",
-  --         }
-  --         return buffer
-  --       end,
-  --     })
-  --   end,
-  -- },
-  -- LazyGit integration with Telescope
-  -- {
-  --   "kdheepak/lazygit.nvim",
-  --   keys = {
-  --     {
-  --       ";c",
-  --       ":LazyGit<Return>",
-  --       silent = true,
-  --       noremap = true,
-  --     },
-  --   },
-  --   -- optional for floating window border decoration
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --   },
-  -- },
-  -- {
-  --   "kristijanhusak/vim-dadbod-ui",
-  --   dependencies = {
-  --     { "tpope/vim-dadbod", lazy = true },
-  --     { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-  --   },
-  --   cmd = {
-  --     "DBUI",
-  --     "DBUIToggle",
-  --     "DBUIAddConnection",
-  --     "DBUIFindBuffer",
-  --   },
-  --   init = function()
-  --     -- Your DBUI configuration
-  --     vim.g.db_ui_use_nerd_fonts = 1
-  --   end,
-  --   keys = {
-  --     {
-  --
-  --       "<leader>d",
-  --       "<cmd>NvimTreeClose<cr><cmd>tabnew<cr><bar><bar><cmd>DBUI<cr>",
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "nvim-tree/nvim-tree.lua",
-  --   config = function()
-  --     require("nvim-tree").setup({
-  --       on_attach = function(bufnr)
-  --         local api = require("nvim-tree.api")
-  --
-  --         local function opts(desc)
-  --           return {
-  --             desc = "nvim-tree: " .. desc,
-  --             buffer = bufnr,
-  --             noremap = true,
-  --             silent = true,
-  --             nowait = true,
-  --           }
-  --         end
-  --
-  --         -- default mappings
-  --         api.config.mappings.default_on_attach(bufnr)
-  --
-  --         -- custom mappings
-  --         vim.keymap.set("n", "t", api.node.open.tab, opts("Tab"))
-  --       end,
-  --       actions = {
-  --         open_file = {
-  --           quit_on_open = true,
-  --         },
-  --       },
-  --       sort = {
-  --         sorter = "case_sensitive",
-  --       },
-  --       view = {
-  --         width = 30,
-  --         relativenumber = true,
-  --       },
-  --       renderer = {
-  --         group_empty = true,
-  --       },
-  --       filters = {
-  --         dotfiles = true,
-  --         custom = {
-  --           "node_modules/.*",
-  --         },
-  --       },
-  --       log = {
-  --         enable = true,
-  --         truncate = true,
-  --         types = {
-  --           diagnostics = true,
-  --           git = true,
-  --           profile = true,
-  --           watcher = true,
-  --         },
-  --       },
-  --     })
-  --
-  --     if vim.fn.argc(-1) == 0 then
-  --       vim.cmd("NvimTreeFocus")
-  --     end
-  --   end,
-  -- },
 }
